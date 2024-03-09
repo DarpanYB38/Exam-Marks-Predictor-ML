d575c94b8d175e2fbc7a7c516c39142748b9249a
Darpan Y B <159637389+DarpanYB38@users.noreply.github.com>
2024-03-09 14:48:53 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, render_template
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        data = request.form
+        hours = data.get('textmarks')
+        age = data.get('textage')
+        internet = data.get('textinternet')
+        if 'buttonpredict' in request.form:
+            path = "/home/darpanyb/Documents/DARPAN/code project/github kt/Data/Exammarks.csv"
+            data = pd.read_csv(path)
+            medianvalue = data.hours.median()
+            data.hours = data.hours.fillna(medianvalue)
+            inputs = data.drop('marks', axis=1)
+            output = data['marks']
+            
+            model = LinearRegression()
+            model.fit(inputs, output)
+            result = model.predict([[float(hours), int(age), int(internet)]])
+            return render_template('index.html', result=result)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
